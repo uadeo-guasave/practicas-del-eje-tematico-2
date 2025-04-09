f2651a58bf57b0c5d1ad9359a530c745dc2bf8cb
Bidkar Aragón Cárdenas <bidkar.aragon@uadeo.mx>
2025-04-09 10:58:59 -0700

    #1 Agregar la función para sumar y la pregunta del proximo viernes

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
 function saludar(nombre) {
-	console.log("Hola " + nombre)
+	console.log("Hola " + nombre + " ¿cómo has estado?")
 }
 
 saludar("Bidkar")
+
+// mi aportacion al issue #1
+function sumar(x,y) {
+	return x+y
+}
+// fin de sumar
